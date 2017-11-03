399ae6a9ad5391773395cc04896067d602e6a3ac
thuallen <yizeng38@gmail.com>
2017-11-03 12:42:53 +0800

    done

--- SA/SA/TSP.cpp
+++ SA/SA/TSP.cpp
@@ -11,6 +11,15 @@
 
 using namespace std;
 
+double myrn01() {
+	double left = 0.0, right = 1.0;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(1, 65536);
+
+	return left + ((double)dist(gen) / 65535) * (right - left);
+}
+
 // transform the coordinate of cities to distance
 void CityDataTranslate() {
 	ifstream read_in;
@@ -106,21 +115,14 @@ TSP_solution SA_TSP(Graph G, std::vector<double> &TSP_y) {
  
 	double Current_Temperature = INITIAL_TEMPERATURE;
 
-	TSP_solution bestSolution;
-	bestSolution.lenOfPath = (double)MAX_INT;
-
-	// origin path 
-	for (int i = 0; i < G.vex_num; i++){
-		bestSolution.path[i] = 'A' + i;
-	}
-
-	random_shuffle(bestSolution.path + 1, bestSolution.path + G.vex_num);
- 
+	TSP_solution bestSolution = findASolution(G);
+	
 	TSP_solution Current_solution;
  
 	while (MIN_TEMPERATURE < Current_Temperature) { 
 		for (int i = 0; i < ITER_NUMBER; i++){
-			Current_solution = FindNewSolution(G, bestSolution);
+			Current_solution = generateNewSolution(G, bestSolution);
+			//cout << Current_solution.lenOfPath<<" "<<bestSolution.lenOfPath << endl;
 			if (Current_solution.lenOfPath <= bestSolution.lenOfPath) {  // accept it 
 				if (Current_solution.lenOfPath == bestSolution.lenOfPath){
 					// cout<<"不同路径出现相同的最优解."<<endl;  
@@ -128,7 +130,9 @@ TSP_solution SA_TSP(Graph G, std::vector<double> &TSP_y) {
 				bestSolution = Current_solution;
 			}else {     
 				double delta = bestSolution.lenOfPath - Current_solution.lenOfPath;
-				if (exp(delta / (Current_Temperature* K)) > rand() * 101){
+				double tmp1 = exp(delta / (Current_Temperature* K)),tmp2 = myrn01();
+ 				//cout << tmp1 << " " << tmp2 << endl;
+ 				if ( tmp1>tmp2 ){
 					bestSolution = Current_solution;
 				}
 			}
@@ -140,44 +144,52 @@ TSP_solution SA_TSP(Graph G, std::vector<double> &TSP_y) {
 	return bestSolution;
 }
 
-void GetRandomArray(int arry[], int number)
-{
+void GetRandomArray(int arry[], int number){
 	int i, k;
 	srand((int)time(0));
-	for (i = 0; i<number - 1; i++)
-	{
+	for (i = 0; i<number - 1; i++){
 		k = rand() % (number - i) + i;
 		swap(arry[i], arry[k]);
 	}
+
 }
 
-TSP_solution FindNewSolution(Graph G, TSP_solution bestSolution) {
-	// generate a new solution  
+void setVal(TSP_solution &s, char p[], double len) {
+	s.lenOfPath = len;
+	int i = 0;
+	for (i = 0; p[i] != '\0'; ++i) {
+		s.path[i] = p[i];
+	}
+	s.path[i] = '\0';
+}
+
+TSP_solution generateNewSolution(Graph G, TSP_solution bestSolution) {
+	// 产生新的解  
 	TSP_solution newSolution;
 
-	int i = rand() % (G.vex_num - 1) + 1;   
+	// 起始城市固定为A, 终点也要返回A, 即需要关注起点A和终点A之间的所有城市  
+	int i = rand() % (G.vex_num - 1) + 1;   // % 取余 -> 即将随机数控制在[1, G.vex_num - 1]  
 	int j = rand() % (G.vex_num - 1) + 1;
 
 	if (i > j){
 		int temp = i;
 		i = j;
 		j = temp;
-	}else if (i == j){				// 表示产生的随机数没有改变的作用, 将此路程设置为最大并结束该函数  
-
+	}else if (i == j){   // 表示产生的随机数没有改变的作用, 将此路程设置为最大并结束该函数  
 		newSolution = bestSolution;
 		return newSolution;
 	}
 
 	/* way 2 */
 	int choose = rand() % 3;
-	if (choose == 0){				// 随机交换任意两个城市的位置  
+	if (choose == 0){   // 随机交换任意两个城市的位置  
 		char temp = bestSolution.path[i];
 		bestSolution.path[i] = bestSolution.path[j];
 		bestSolution.path[j] = temp;
-	}else if (choose == 1){			// 随机逆置城市的位置  
+	}else if (choose == 1){   // 随机逆置城市的位置  
 		reverse(bestSolution.path + i, bestSolution.path + j);
-	}else {							// 随机移位城市的位置  
-		if (j + 1 == G.vex_num) {	//边界处不处理    
+	}else {   // 随机移位城市的位置  
+		if (j + 1 == G.vex_num) { //边界处不处理    
 			newSolution = bestSolution;
 			return newSolution;
 		}
@@ -195,8 +207,8 @@ double CalculateLength(Graph G, TSP_solution newSolution) {
 	double _length = 0.0;
 
 	for (int i = 0; i < G.vex_num - 1; i++){
-		int _startCity = (int)newSolution.path[i] - 'A';
-		int _endCity = (int)newSolution.path[i + 1] - 'A';
+		int _startCity = (int)newSolution.path[i] - 65;
+		int _endCity = (int)newSolution.path[i + 1] - 65;
 		if (G.arcs[_startCity][_endCity] == -1){
 			return MAX_INT;
 		}else {
@@ -205,14 +217,44 @@ double CalculateLength(Graph G, TSP_solution newSolution) {
 	}
 
 	// 判断该路径是否能回到起始城市  
-	if (G.arcs[(int)newSolution.path[G.vex_num - 1] - 'A'][(int)newSolution.path[0] - 'A'] == -1){
+	if (G.arcs[(int)newSolution.path[G.vex_num - 1] - 65][(int)newSolution.path[0] - 65] == -1){
 		return MAX_INT;
 	}else {
-		_length += G.arcs[(int)newSolution.path[G.vex_num - 1] - 'A'][(int)newSolution.path[0] - 'A'];
+		_length += G.arcs[(int)newSolution.path[G.vex_num - 1] - 65][(int)newSolution.path[0] - 65];
 
 		return _length;
 	}
 
+}
+
+TSP_solution findASolution(Graph G){
+	int n = G.vex_num;
+	int tmp[MAX_VEXNUM];
+	char mpath[MAX_VEXNUM];
+
+	mpath[0] = 'A';
+	mpath[n] = 'A';
+	for (int i = 0; i < n-1; ++i) {		
+		tmp[i] = i + 1;
+	}
+	
+	int num = n - 1;
+	GetRandomArray(tmp, num);
+	for (int i = 1; i <= n - 1; ++i) {
+		mpath[i] = tmp[i - 1]+'A';
+	}
+
+	mpath[n + 1] = '\0';
+	TSP_solution slt;
+	double len = 0.0;
+	
+	for (int i = 0; i < n; ++i) {
+		len += G.arcs[mpath[i] - 'A'][mpath[i + 1] - 'A'];
+	}
+
+	setVal(slt, mpath, len);
+
+	return slt;
 }
 
 void Display(Graph G, TSP_solution bestSoluion) {
--- SA/SA/TSP.h
+++ SA/SA/TSP.h
@@ -2,13 +2,14 @@
 #define MAX_VEXNUM 30  
 #define MAX_CITYNUM 150  
 #include <vector>
+#include <random>
 
 const int ITER_NUMBER = 100;
 const double SPEED = 0.98;                   
-const double INITIAL_TEMPERATURE = 50000.0;   
-const double MIN_TEMPERATURE = 100.0;        
+const double INITIAL_TEMPERATURE = 5000.0;   
+const double MIN_TEMPERATURE = 10.0;        
 const int MAX_INT = 999999;
-const double K = 2.0;
+const double K = 0.0005;
 
 typedef struct {
 	int vex_num, arc_num;                    
@@ -19,11 +20,11 @@ typedef struct {
 typedef struct {
 	double lenOfPath;
 	char path[MAX_VEXNUM];
-	
 }TSP_solution;
 
 void CreateGraph(Graph &G);
 TSP_solution SA_TSP(Graph G, std::vector<double> &TSP_y);
-TSP_solution FindNewSolution(Graph G, TSP_solution bestSolution);
+TSP_solution findASolution(Graph G);
+TSP_solution generateNewSolution(Graph G, TSP_solution bestSolution);
 double CalculateLength(Graph G, TSP_solution newSolution);
 void Display(Graph G, TSP_solution bestSoluion);
